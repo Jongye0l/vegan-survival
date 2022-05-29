476d4ecc3a7e5d5cddbea014d66dabde2ac87f1c
Jongyeol <45934107+Jongye0l@users.noreply.github.com>
2022-05-29 16:45:56 +0900

    1.2.1

--- src/com/Jongyeol/vegan/Notification/madeby.java
+++ src/com/Jongyeol/vegan/Notification/madeby.java
@@ -13,12 +13,11 @@ public class madeby extends BukkitRunnable {
     public static void madebyJongyeol() {
         Bukkit.broadcastMessage(ChatColor.AQUA + "---------------------------------------");
         Bukkit.broadcastMessage(ChatColor.GREEN + "해당 플러그인은 종열이 제작하였습니다.");
-        Bukkit.broadcastMessage(ChatColor.GREEN + "해당 플러그인의 모든 저작권은 종열에게 있습니다.");
-        Bukkit.broadcastMessage(ChatColor.GREEN + "해당 플러그인은 종열외의 배포, 판매, 공유를 금합니다.");
-        Bukkit.broadcastMessage(ChatColor.GREEN + "Copyright 2022. Jongyeol All rights reserved.");
-        TextComponent text = new TextComponent(ChatColor.GREEN + "자세한 사항은 종열의 Github를 확인해주세요.");
-        text.setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://github.com/Jongye0l/vegan-survival"));
-        text.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder("클릭 시 깃허브에 접속합니다.").create()));
+        Bukkit.broadcastMessage(ChatColor.GREEN + "Copyright (c) 2022, Jongyeol All rights reserved.");
+        Bukkit.broadcastMessage(ChatColor.GREEN + "해당 플러그인의 BSD 3-Clause 라이선스를 가지고 있습니다.");
+        TextComponent text = new TextComponent(ChatColor.GREEN + "자세한 내용은 github에서 확인해주세요.");
+        text.setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://github.com/Jongye0l/vegan-survival/blob/main/LICENSE"));
+        text.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder("클릭 시 라이선스를 확인합니다.").create()));
         for (Player player : Bukkit.getOnlinePlayers()){
             player.spigot().sendMessage(text);
         }
